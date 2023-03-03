23a940eb65d58e39d9f2113d4128f3d1b11ea8b1
hainet50b <20400616+hainet50b@users.noreply.github.com>
2023-03-04 00:04:44 +0900

    UCPでDBCPを作成してコネクションを取得するサンプルを作成した。

---
+++ oracle-ucp-api-gym/src/main/java/com/programacho/OracleUcpApiGymApplication.java
@@ -0,0 +1,54 @@
+package com.programacho;
+
+import oracle.ucp.jdbc.PoolDataSource;
+import oracle.ucp.jdbc.PoolDataSourceFactory;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.UUID;
+
+public class OracleUcpApiGymApplication {
+
+    public static void main(String[] args) {
+        try {
+            PoolDataSource ds = PoolDataSourceFactory.getPoolDataSource();
+
+            ds.setConnectionFactoryClassName("org.h2.Driver");
+            ds.setURL("jdbc:h2:mem:oracle-ucp-api-gym");
+            ds.setUser("sa");
+            ds.setPassword("");
+
+            ds.setConnectionPoolName("PROGRAMACHO_POOL");
+            ds.setMaxPoolSize(30);
+            ds.setMinPoolSize(30);
+
+            ds.setValidateConnectionOnBorrow(true);
+            ds.setSQLForValidateConnection("SELECT 1 FROM DUAL");
+
+            Connection conn = ds.getConnection();
+
+            try (Statement createTable = conn.createStatement()) {
+                createTable.execute("CREATE TABLE emp (id VARCHAR);");
+            }
+
+            try (PreparedStatement insert = conn.prepareStatement("INSERT INTO emp VALUES (?);")) {
+                for (int i = 0; i < 10; i++) {
+                    insert.setString(1, UUID.randomUUID().toString());
+                    insert.execute();
+                }
+            }
+
+            try (Statement stmt = conn.createStatement()) {
+                ResultSet rs = stmt.executeQuery("SELECT * FROM emp;");
+                while (rs.next()) {
+                    System.out.println(rs.getString(1));
+                }
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
